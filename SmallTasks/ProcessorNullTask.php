95af4fa21bbe34767fbd23dbc0199e854b42a0c9
FGasquez <federicogasquez@gmail.com>
2018-09-30 18:35:48 -0300

    Added ProcessorNullTask

---
+++ SmallTasks/ProcessorNullTask.php
@@ -0,0 +1,30 @@
+<?php namespace SmallTasks;
+
+/**
+ * AbstractProcessorTask
+ * 
+ * @package smalltasks
+ * @author Ulises Jeremias Cornejo Fandos <ulisescf.24@gmail.com>
+ * @author Federico Ramon Gasquez <federicogasquez@gmail.com>
+ */
+abstract class ProcessorNullTask extends AbstractProcessorTask 
+{
+
+    private $previous;
+
+    public function __contruct(AbstractProcessorTask $processorTask){
+        $this->previous = $processorTask;
+    }
+
+    /*
+     * @inheritDoc
+     */
+    public function handle($elem) {
+        return $elem;
+    }
+
+    public function add(AbstractProcessorTask $processorTask){
+        $this->previous->setSucessor($processorTask);
+        return $this;
+    }
+}
